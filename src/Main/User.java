cbb61e41029ec71a29f7175b2ff64dfca5994fd7
Umayrah <umayrah.chonee@mail.utoronto.ca>
2021-10-13 17:08:32 -0400

    added all the classes that were on the CRC cards.

--- src/Main/User.java
+++ src/Main/User.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+
+    // variables
+    private String username;
+    private boolean isVerified;
+    private List<Post> listPosts;
+    private List<User> listFollowing;
+    private List<Product> shoppingCart;
+    private List<Product> currentSearches;
+
+    /**
+     * Creates a new User object.
+     * This constructor takes in  arguments, a String for the Bag's colour and
+     * an int for the Bag's capacity.
+     *
+     * @param username this user's username.
+     */
+
+    public User(String username){
+        this.username = username;
+        this.isVerified = false;
+        this.listPosts = new ArrayList<>();
+        this.shoppingCart = new ArrayList<>();
+        this.currentSearches = new ArrayList<>();
+    }
 }
