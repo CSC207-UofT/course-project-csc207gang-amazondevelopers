f973803ee1e45e05d671e11ae2d1e8fcf996f084
Yasamin <yasamin.nourijelyani@mail.utoronto.ca>
2021-10-13 17:01:15 -0400

    I created the methods and attributes for master

--- src/Main/Master.java
+++ src/Main/Master.java
@@ -1,2 +1,32 @@
+import java.util.HashMap;
+
+
 public class Master {
+
+    private HashMap<String, User> userDict = new HashMap<>();
+    private HashMap<String, Product> productDict = new HashMap<>();
+
+
+
+
+    public HashMap<String, User> getterUser(){
+        return userDict;
+    }
+
+    public HashMap<String, Product> getterProduct(){
+        return productDict;
+    }
+
+
+    public void setterUser(String username, User userProfile){
+        userDict.put(username, userProfile);
+    }
+
+    public void setterProduct(String productID, Product item){
+        productDict.put(productID, item);
+
+    }
+
+
+
 }
