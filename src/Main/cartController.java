69da52910e4ef3346a12310fbe8fd549c8455ef0
Jacqueline Zhu <jacqueline.zhu@mail.utoronto.ca>
2021-10-16 16:24:59 -0400

    updated code for createUserController, new code for cartController

--- src/Main/cartController.java
+++ src/Main/cartController.java
@@ -1,2 +1,41 @@
+import InputAndOutput.InOut;
+
+import java.io.IOException;
+/**
+ * A controller class that adds items to a users cart.
+ */
 public class cartController {
+    private userManager userManager = new userManager();
+
+    public void addToCart(InOut inOut, User user) {
+        inOut.sendOutput("What product would you like to buy? (Input index):");
+        try {
+            String stringIndex = inOut.getInput();
+            try{
+                // turn string into an int, exception is thrown when the string entered cannot be turned to an int
+                int productIndex = Integer.parseInt(stringIndex);
+                boolean validIndex = false;
+                while(!validIndex){
+                    if (userManager.addToShoppingCartSearches(productIndex, user)){
+                        validIndex = true;
+                        inOut.sendOutput("Successfully added to cart.");
+                    }
+                    else{
+                        inOut.sendOutput("Error, please enter a valid index.");
+                        stringIndex = inOut.getInput();
+                        productIndex = Integer.parseInt(stringIndex);
+                    }
+                }
+
+            }
+            catch(NumberFormatException e){
+                // exception thrown when user inputs a string that is not a number
+                inOut.sendOutput("An error occurred, please enter a number.");
+                // process for buying an item should restart
+            }
+
+        } catch (IOException e) {
+            inOut.sendOutput("An error occurred, try again.");
+        }
+    }
 }
