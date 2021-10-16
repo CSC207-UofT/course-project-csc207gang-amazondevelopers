c3e08ca5717bf54e208cad2f5a9a7c9c227f8554
Jacqueline Zhu <jacqueline.zhu@mail.utoronto.ca>
2021-10-16 16:48:08 -0400

    updated code for createUserController, code for cartController

--- src/Main/cartController.java
+++ src/Main/cartController.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class cartController {
     private userManager userManager = new userManager();
 
-    public void addToCart(InOut inOut, User user) {
+    public void addToCartSearch(InOut inOut, User user) {
         inOut.sendOutput("What product would you like to buy? (Input index):");
         try {
             String stringIndex = inOut.getInput();
@@ -18,6 +18,39 @@ public class cartController {
                 while(!validIndex){
                     // try and add the product at the index to the users cart
                     if (userManager.addToShoppingCartSearches(productIndex, user)){
+                        validIndex = true;
+                        inOut.sendOutput("Successfully added to cart.");
+                    }
+                    else{
+                        inOut.sendOutput("Error, please enter a valid index.");
+                        stringIndex = inOut.getInput();
+                        productIndex = Integer.parseInt(stringIndex);
+                    }
+                }
+
+            }
+            catch(NumberFormatException e){
+                // exception thrown when user inputs a string that is not a number
+                inOut.sendOutput("An error occurred, please enter a number.");
+                // process for buying an item should restart
+            }
+
+        } catch (IOException e) {
+            inOut.sendOutput("An error occurred, try again.");
+        }
+    }
+
+    public void addToCartBrowse(InOut inOut, User user){
+        inOut.sendOutput("What product would you like to buy? (Input index):");
+        try {
+            String stringIndex = inOut.getInput();
+            try{
+                // turn string into an int, exception is thrown when the string entered cannot be turned to an int
+                int productIndex = Integer.parseInt(stringIndex);
+                boolean validIndex = false;
+                while(!validIndex){
+                    // try and add the product at the index to the users cart
+                    if (userManager.addToShoppingCartFeed(productIndex, user)){
                         validIndex = true;
                         inOut.sendOutput("Successfully added to cart.");
                     }
