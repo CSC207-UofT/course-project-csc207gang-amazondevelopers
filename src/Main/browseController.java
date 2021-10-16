86f34d5c99554ccc48eec05ab7e7a7fa93935aeb
Diego Gonzalez <diego.gonzalezdelgado@mail.utoronto.ca>
2021-10-16 12:14:00 -0400

    First skeleton for user Options Controller

--- src/Main/browseController.java
+++ src/Main/browseController.java
@@ -1,2 +1,17 @@
 public class browseController {
+
+    public String User() throws Exception{
+        try {
+            var x = "";
+            // for loop here with information drawn from users this User() is followingw
+            return x;
+
+            throw new Exception("That user does not exist, try again!");
+            // throw exception if the user doesn't exist
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- src/Main/userOptionsController.java
+++ src/Main/userOptionsController.java
@@ -1,37 +1,30 @@
-import java.util.ArrayList;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class userOptionsController {
 
     Scanner input = new Scanner(System.in);
 
-    ArrayList<String> options = new ArrayList<>();
-    options.add("post");
-    options.add("browse");
-    options.add("search");
-
-    public void userInput() throws Exception {
+    public Object userInput() throws Exception {
 
         System.out.println("What would you like to do? Input one of Search, Post, Browse");
-        String userDecision = input.nextLine();
+        String userDecision = input.nextLine().toLowerCase();
 
-
-        if (!(options.contains(userDecision.toLowerCase(Locale.ROOT)))){
-        throw new Exception("That is not an accepted input, please try again!");
-        // in case the input is not in the list of accepted inputs, ends early
-        }
-        else if(userDecision.equals("post")){
-            createPostController createPostController = new createPostController();
-            // redirects to createPostController class
-        }
-        else if(userDecision.equals("browse")){
-            browseController browseController = new browseController();
-            // redirects to browseController and return feed
-        }
-        else {
-            searchController searchController = new searchController();
-            // redirects to search and returns relevant search info
+        try{
+            switch (userDecision) {
+                case "post":
+                    // redirects to createPostController class
+                    return new createPostController();
+                case "browse":
+                    // redirects to browseController and return feed
+                    return new browseController();
+                case "search":
+                    // redirects to searchController and returns relevant search info
+                    return new searchController();
+            }
+            throw new Exception("That is not an accepted input, please try again!");
+            // throws exception in case the input is not in the available options of inputs
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
