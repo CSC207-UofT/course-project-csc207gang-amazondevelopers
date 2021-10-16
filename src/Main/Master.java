a802d3a0b291edc3737b1facfb69f69593de55ef
Yasamin <yasamin.nourijelyani@mail.utoronto.ca>
2021-10-15 21:21:00 -0400

    added attributes and methods to master, masterManager, and search controller. Also created a main

--- src/Main/Master.java
+++ src/Main/Master.java
@@ -1,31 +1,19 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
-
+/**
+ * A class that store all users and products in the marketplace
+ */
 public class Master {
-
-    private HashMap<String, User> userDict = new HashMap<>();
-    private HashMap<String, Product> productDict = new HashMap<>();
-
-
-
-
-    public HashMap<String, User> getterUser(){
-        return userDict;
-    }
-
-    public HashMap<String, Product> getterProduct(){
-        return productDict;
-    }
+    // a dictionary mapping the string usernames to users
+    public static HashMap<String, User> userDict = new HashMap<>();
+    // a dictionary mapping the string product IDs to products
+    public static HashMap<String, Product> productDict = new HashMap<>();
+    // a dictionary mapping the string tag word to list of products
+    public static HashMap<String, ArrayList<Product>> productTagMap = new HashMap<>();
 
 
-    public void setterUser(String username, User userProfile){
-        userDict.put(username, userProfile);
-    }
 
-    public void setterProduct(String productID, Product item){
-        productDict.put(productID, item);
-
-    }
 
 
 
--- src/Main/masterManager.java
+++ src/Main/masterManager.java
@@ -1,2 +1,49 @@
+import java.util.ArrayList;
+
+/**
+ * A use case class that accesses and modulated manager entity.
+ */
 public class masterManager {
+
+
+    /**
+     * A getter of a user from the User dictionary. Which has the username of the user as Strings and the User as the value.
+     * @return the userDict
+     */
+    public static User getterUser(String username){
+        return Master.userDict.get(username);
+    }
+
+    /**
+     * A getter of the products dictionary. Which has the ID of the product of the user as Strings
+     * and the Product as the value.
+     * @return porductDict
+     */
+    public static Product getterProduct(String productID){
+        return Master.productDict.get(productID);
+    }
+
+    /**
+     * Inputs a new user in the userDict by modifying the dictionary.
+     * @param username  the username of the user
+     * @param userProfile  the User associated with the username
+     */
+    public static void setterUser(String username, User userProfile){
+        Master.userDict.put(username, userProfile);
+    }
+
+
+    /**
+     * Inputs a new product in the productDict by modifying the dictionary.
+     * @param productID   the ID of the product
+     * @param item     the item type Product that a User has added to the market.
+     */
+    public static void setterProduct(String productID, Product item){
+        Master.productDict.put(productID, item);
+
+    }
+
+    public static ArrayList<Product> getSearchList(String tagWord) {
+        return Master.productTagMap.get(tagWord);
+    }
 }
