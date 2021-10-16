ee4bbfbb37dd0084291ef7eacafd8b3d5f734ca0
samgu <sam.gunn@mail.utoronto.ca>
2021-10-15 23:21:00 -0400

    Post andPost manager

--- src/Main/postManager.java
+++ src/Main/postManager.java
@@ -4,9 +4,10 @@ public class postManager {
     private userManager usermanager = new userManager();
     /** create a post and the product it is based on, add this post to the postList in the user that created it **/
     public Post createPost (ArrayList<String > t, User u, String d, objects to initialize product){
-        private Product p = productmanager.createProduct(variables);
+        private Product p = productmanager.createProduct(variables);/*need to know what variables to create a product are*/
         private Post post = new Post(p,t,d,u);
         usermanager.addToPostList(u,post); /* need userManager to implement this method*/
+        return post;
     }
 }
 
