d8964863fded6272a140fc5270427d552329185a
Jacqueline Zhu <jacqueline.zhu@mail.utoronto.ca>
2021-10-16 20:05:43 -0400

    added javadoc strings to cartcontroller and createusercontroller

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,4 +1,15 @@
+import InputAndOutput.SystemInOut;
+
 public class Main {
 
+    public static void main(String[] args) {
+        SystemInOut inOut = new SystemInOut();
+        userOptionsController options = new userOptionsController();
+        try {
+            options.userInput();
+        } catch(Exception e){
+            inOut.sendOutput("error occurred");
+        }
 
+    }
 }
