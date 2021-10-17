b1c19256c2b7c5dc45a28b3e16134f2667a0572e
Jacqueline Zhu <jacqueline.zhu@mail.utoronto.ca>
2021-10-16 20:48:04 -0400

    put code in main, edited search and options controller

--- src/Main/searchController.java
+++ src/Main/searchController.java
@@ -1,4 +1,8 @@
+import InputAndOutput.InOut;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * take user input from command line interface and
@@ -10,13 +14,86 @@ import java.util.ArrayList;
 
 public class searchController {
 
+
+    public void searchProducts(InOut inOut){
+        boolean validTag = false;
+        String tagWord;
+        masterManager masterManager = new masterManager();
+
+        inOut.sendOutput("Search by tag: ");
+        try{
+            // code to enforce a limit to the length of the username, 20 characters max
+            while (!validTag){
+                tagWord = inOut.getInput();
+                // check if the tage word is valid
+
+                // fix this
+                List<> searchList = masterManager.getSearchList(tagWord);
+
+                if ()
+
+                    /* ignore this, this is just for reference
+                if (username.length() <= 20 && userManager.createUser(username)){
+                    validUsername = true;
+                    inOut.sendOutput("User profile successfully created.");
+                    // print out username for the user to look over
+                    inOut.sendOutput("Username: " + username);
+                }
+                else if (username.length() > 20){
+                    inOut.sendOutput("Username is too long. Try again.");
+                }
+                else{
+                    inOut.sendOutput("Username has already been taken. Try again.");
+                }
+            }
+            */
+
+
+            /* This code will be uncommented later, will not be included in skeleton
+            inOut.sendOutput("Input password:");
+            String password = inOut.getInput();
+
+            inOut.sendOutput("Input profile description:");
+            // should also try and enforce a limit to the # of characters in the description
+            String description = inOut.getInput();
+
+            // need code to get user profile picture
+            */
+
+                /*
+        } catch (IOException e) {
+            inOut.sendOutput("An error occurred, try again.");
+            // maybe restart user creation process over again if exception is thrown
+        }
+        // ideas: give user an option to redo user creation
+        */
+
+
+
+
+
+
+
+
+
+
+        if (masterManager.getSearchList(tagWord).equals(false)){
+            return false;}
+        else{
+            return masterManager.getSearchList(tagWord);
+
+
+        }
+
+
+
     public Object getSearchItems(String tagWord){
 
         if (masterManager.getSearchList(tagWord).equals(false)){
             return false;}
         else{
-
             return masterManager.getSearchList(tagWord);
+
 
         }
     }
