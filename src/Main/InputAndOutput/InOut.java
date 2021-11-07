2c6d716fa4c726f0a592bac5bada7cdc6a558e99
Yasamin <yasamin.nourijelyani@mail.utoronto.ca>
2021-11-07 18:49:43 -0500

    added SystemInOut back

---
+++ src/Main/InputAndOutput/InOut.java
@@ -0,0 +1,13 @@
+package InputAndOutput;
+
+import java.io.IOException;
+
+/**
+ * An interface that interacts with user input. to be implemented by SystemInOut.
+ */
+
+public interface InOut {
+
+    String getInput() throws IOException;
+    void sendOutput(Object s);
+}
