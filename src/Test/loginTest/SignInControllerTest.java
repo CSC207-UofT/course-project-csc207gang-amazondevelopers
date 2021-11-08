bf6da620e8628404c0dc7f4b34a03318dbdf1ea0
Diego <diego.gonzalezdelgado@mail.utoronto.ca>
2021-11-08 11:10:56 -0500

    test for existing user added test for a user that does not exist

--- src/Test/loginTest/SignInControllerTest.java
+++ src/Test/loginTest/SignInControllerTest.java
@@ -1,4 +1,37 @@
 package loginTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SignInControllerTest {
+import InputAndOutput.SystemInOut;
+import UserFunctions.User;
+import login.SignInController;
+import login.SignUpController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class SignInControllerTest extends SignInController{
+
+    SignInController signin;
+    SignUpController signup;
+    User user;
+    SystemInOut inOut;
+
+    @BeforeEach
+    void setUp() throws IOException, ClassNotFoundException {
+
+        signin = new SignInController();
+        signup = new SignUpController();
+        signup.allowSignUp("test1");
+        user = new User("test1");
+
+    }
+    @Test
+    void testSignInReal() throws IOException, ClassNotFoundException {
+        assertEquals(signin.allowSignIn("test1", inOut).getUsername(), "test1");
+    }
+    @Test
+    void testSignInFake() throws IOException, ClassNotFoundException{
+        assertEquals(signin.allowSignIn("test2", inOut).getUsername(), "");
+    }
 }
