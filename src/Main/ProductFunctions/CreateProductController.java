2becc62200b5a9599ad7c6c00c4aded3dcd2910b
Umayrah <umayrah.chonee@mail.utoronto.ca>
2021-11-09 23:17:05 -0500

    Finished refactoring ProductManager class. Added two new gateway interfaces and classes that implement them. Added a new .ser file to store a dictionary mapping product id to Product. Contributors: Albert: aj.luo@mail.utoronto.ca Umayrah: umayrah.chonee@mail.utoronto.ca

--- src/Main/ProductFunctions/CreateProductController.java
+++ src/Main/ProductFunctions/CreateProductController.java
@@ -3,38 +3,99 @@ package ProductFunctions;
 import InputAndOutput.SystemInOut;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Objects;
+
+import Undo.Undo;
+import UserFunctions.User;
+import OptionsPackage.UserOptionsController;
 
 public class CreateProductController {
 
-    public Product createNewProductFromInput(SystemInOut input) throws IOException, ClassNotFoundException {
+    public Product createNewProductFromInput(SystemInOut input, User user) throws Exception {
+        Undo undo = new Undo();
+        undo.addData("Name");
+        undo.addData("ID");
+        undo.addData("Price");
+        undo.addData("Category");
+        undo.addData("Quantity");
+        undo.addData("Size");
+        undo.addState("Name");
+        undo.addState("ID");
+        undo.addState("Price");
+        undo.addState("Category");
+        undo.addState("Quantity");
+        undo.addState("Size");
+        while(!undo.isComplete()) {
+            if (Objects.equals(undo.getCurrentState(), "Name")) {
+                input.sendOutput("What is the name of the product?");
+                String name = input.getInput();
+                if (name.equals("*")){
+                    UserOptionsController uo = new UserOptionsController(user);
+                    uo.userInput(input);
+                    throw new IOException();
+                }
+                else{
+                    undo.setDataPoint(name);
+                }
+            }
+            if (Objects.equals(undo.getCurrentState(), "ID")) {
+                input.sendOutput("What is the ID of the product?");
+                String ID = input.getInput();
+                if (ID.equals("*")){
+                    undo.undo();
+                }
+                else{
+                    undo.setDataPoint(ID);
+                }
+            }
+            if (Objects.equals(undo.getCurrentState(), "Price")) {
+                input.sendOutput("What is the price of this product?");
+                String priceString = input.getInput();
+                if (priceString.equals("*")){
+                    undo.undo();
+                }
+                else{
+                    Double price = Double.parseDouble(priceString);
+                    undo.setDataPoint(price);
+                }
 
-        input.sendOutput("What is the name of the product?");
-        String name = input.getInput();
+            }
+            if (Objects.equals(undo.getCurrentState(), "Category")) {
+                input.sendOutput("What is the Category of this product?");
+                String category = input.getInput();
+                if (category.equals("*")){
+                    undo.undo();
+                }
+                else{
+                    undo.setDataPoint(category);
+                }
+            }
 
-        input.sendOutput("What is the ID of the product?");
-        String ID = input.getInput();
+            if (Objects.equals(undo.getCurrentState(), "Quantity")) {
+                input.sendOutput("What is the quantity of this product? Please enter an integer");
+                String quantityString = input.getInput();
+                if (quantityString.equals("*")){
+                    undo.undo();
+                }
+                else {
+                    int quantity = Integer.parseInt(quantityString);
+                    undo.setDataPoint(quantity);
+                }
+            }
+            if (Objects.equals(undo.getCurrentState(), "Size")) {
+                input.sendOutput("What is the size of this product? Press enter No Size if this product does not have a size");
+                String sizeInput = input.getInput();
+            }
+        }
+        HashMap<String, Object> output = undo.get_Data();
 
-        input.sendOutput("What is the price of this product?");
-        String priceString = input.getInput();
-        Double price = Double.parseDouble(priceString);
-
-        input.sendOutput("What is the Category of this product?");
-        String category = input.getInput();
-
-        input.sendOutput("What is the size of this product? Press enter if this product does not have a size");
-        String sizeInput = input.getInput();
-
-
-        input.sendOutput("What is the quantity of this product? Please enter an integer");
-        String quantityString = input.getInput();
-        int quantity = Integer.parseInt(quantityString);
-
-        if (! sizeInput.equals("")){
+        if (! output.get("Size").equals("No Size")){
             ProductManager productManager = new ProductManager();
-            return productManager.createProduct(name, ID, price, category, sizeInput, quantity);
+            return productManager.createProduct((String)output.get("Name"), (String)output.get("ID"), (double)output.get("Price"),(String) output.get("Category"), (String)output.get("Size"),(int) output.get("Quantity"));
         }else{
             ProductManager productManager = new ProductManager();
-            return productManager.createProduct(name, ID, price, category, quantity);
+            return productManager.createProduct((String)output.get("Name"), (String)output.get("ID"), (double)output.get("Price"),(String) output.get("Category"),(int) output.get("Quantity"));
         }
 
     }
