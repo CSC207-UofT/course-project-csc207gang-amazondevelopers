288caf0c2c1632b8d45d3daea7ea4cd98b2092f4
Diego <diego.gonzalezdelgado@mail.utoronto.ca>
2021-11-10 09:35:41 -0500

    added basic test to SearchControllerTest + couple skeletons for tests

--- src/Test/OptionsPackageTest/SearchControllerTest.java
+++ src/Test/OptionsPackageTest/SearchControllerTest.java
@@ -1,4 +1,35 @@
 package OptionsPackageTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class SearchControllerTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import OptionsPackage.SearchController;
+
+public class SearchControllerTest extends SearchController {
+    SearchController searchController;
+
+    @BeforeEach
+    void setUp(){
+        searchController = new SearchController();
+    }
+
+    @Test
+    void testNotInSearch() throws IOException, ClassNotFoundException {
+        String tag = new String("test1");
+        ArrayList<Object> list = new ArrayList<>();
+        assertEquals(searchController.searchProducts(tag), list);
+    }
+
+    void testThrowsException() throws IOException, ClassNotFoundException{
+        ;
+    }
+
+    void testInSearch() throws IOException, ClassNotFoundException{
+        ;
+    }
 }
--- src/Test/loginTest/SignInControllerTest.java
+++ src/Test/loginTest/SignInControllerTest.java
@@ -1,6 +1,5 @@
 package loginTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import InputAndOutput.SystemInOut;
 import UserFunctions.User;
