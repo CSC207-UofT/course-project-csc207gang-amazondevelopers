f7434201c81521e7f657bbe2846c9a0662d31011
Jacqueline Zhu <jacqueline.zhu@mail.utoronto.ca>
2021-11-10 22:46:41 -0500

    added comments to BuyController for clarity, some minor edits

--- src/Main/OptionsPackage/BuyController.java
+++ src/Main/OptionsPackage/BuyController.java
@@ -20,10 +20,11 @@ public class BuyController {
         List<Product> productsOfInterest = search.allowSearch(input);
         presenter.presentTagList(productsOfInterest, input);
 
-        input.sendOutput("Would you like to purchase one of the items?" +
-                "enter the number of your choice\n 1.Yes\n2.No\nType 'exit' if you would like to exit search.");
-        boolean keepChecking = true;
-        while(keepChecking) {
+        // loop to keep checking for the users answer to the output message right below
+        boolean keepRunning = true;
+        while(keepRunning) {
+            input.sendOutput("Would you like to purchase one of the items?" +
+                    "enter the number of your choice\n 1.Yes\n2.No\nType 'exit' if you would like to exit.");
             String decisionToBuy = input.getInput();
 
             if (decisionToBuy.equals("1")) {
@@ -32,7 +33,7 @@ public class BuyController {
                 while (!boughtOrExit) {
                     input.sendOutput("Please select the index of the item that you want to buy. The index is the " +
                             "integer value for the position of the item on the list, with the first item being at " +
-                            "the 0th index. Type exit if you would like to go back to your search result.");
+                            "the 0th index. Type 'exit' if you would like to go back to your search result.");
                     String itemIndex = input.getInput();
                     int indexInt = Integer.parseInt(itemIndex);
                     if (indexInt < productsOfInterest.size()) {
@@ -40,19 +41,25 @@ public class BuyController {
                         // add this product to the cart
                         CartManager cart = new CartManager();
                         cart.addToCart(itemOfInterest, user);
+                        // the user has bought something, so user can now decide if they want to buy something
+                        // else from the search results or not (returned to outer while loop)
                         boughtOrExit = true;
                     } else if (itemIndex.equals("exit")) {
+                        // return to the outer while loop so user can decide if they want to do something
+                        // else other than buy
                         boughtOrExit = true;
                     } else {
                         input.sendOutput("incorrect index, try again");
+                        // user needs to type in a new index/input in next iteration of this loop
                     }
                 }
-
             } else if (decisionToBuy.equals("2")) {
+                // let the user search for something new if they dont want to buy something
                 search.allowSearch(input);
-                keepChecking = false;
+                keepRunning = false;
             } else {
-                return;
+                // end the loop, thereby ending the call to BuyController
+                keepRunning = false;
             }
         }
 
