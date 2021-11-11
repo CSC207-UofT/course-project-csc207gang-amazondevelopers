347c97d72ba9358465004826cda0105a074a0719
Jacqueline Zhu <jacqueline.zhu@mail.utoronto.ca>
2021-11-11 15:36:00 -0500

    SystemInOutTest can now take in different filenames, other edits

--- src/Test/SystemInOutTest.java
+++ src/Test/SystemInOutTest.java
@@ -1,36 +1,38 @@
 import InputAndOutput.InOut;
+import ProductFunctions.Product;
 
 import java.io.*;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 public class SystemInOutTest implements InOut {
+
+    private final Scanner reader;
+
     /**
      *
      * @param fileName
      * @throws FileNotFoundException
      */
-
     public SystemInOutTest(String fileName) throws FileNotFoundException {
+        File fileToRead = new File(fileName);
+        this.reader = new Scanner(fileToRead);
     }
-
-
-    public void scan(){}
-
-    File newfile = new File("src/Test/getUsername.txt");
-    Scanner myReader = new Scanner(newfile);
 
     @Override
     public String getInput() throws IOException {
-        String data = myReader.nextLine();
-        return data;
-
+        if (reader.hasNextLine()) {
+            return this.reader.nextLine();
+        }
+        else{
+            return "";
+        }
     }
 
     @Override
     public void sendOutput(Object s) {
         System.out.println(s);
-
     }
 
-}//
+}
