703762c1366e15a32190be82f930a921b930edcb
Yasamin <yasamin.nourijelyani@mail.utoronto.ca>
2021-11-14 19:44:58 -0500

    testing gateways aj.luo@mail.utoronto.ca yasamin.nourijelyani@mail.utoronto.ca

--- src/Test/loginTest/SignInGatewayTest.java
+++ src/Test/loginTest/SignInGatewayTest.java
@@ -1,4 +1,30 @@
 package loginTest;
 
+import Settings.DeleteUserGateway;
+import UserFunctions.User;
+import login.GetUserGateway;
+import login.SignUpGateway;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
 public class SignInGatewayTest {
+
+    GetUserGateway getUserGateway = new GetUserGateway();
+    SignUpGateway signUpGateway = new SignUpGateway();
+    DeleteUserGateway deleteUserGateway = new DeleteUserGateway();
+
+    @Test
+    public void getUserBasicTest() throws IOException, ClassNotFoundException {
+        User newUser = new User("albert");
+        signUpGateway.allowSignUp(newUser.getUsername(), newUser);
+
+        User expectedUser = getUserGateway.getUser("albert");
+        assertEquals(newUser.getUsername(), expectedUser.getUsername());
+        deleteUserGateway.deleteUser("albert");
+
+    }
+
 }
