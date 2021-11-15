7616541e1465f8bfcfc0d243b9e97be7ff4195f1
Diego <diego.gonzalezdelgado@mail.utoronto.ca>
2021-11-14 22:12:03 -0500

    added more test files and bug fixes

--- src/Test/OptionsPackageTest/ListOfProductsPresenterTest.java
+++ src/Test/OptionsPackageTest/ListOfProductsPresenterTest.java
@@ -1,5 +1,44 @@
 package OptionsPackageTest;
+import InputAndOutput.SystemInOut;
+import PostFunctions.Post;
+import ProductFunctions.Product;
+import loginTest.SignInControllerTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import UserFunctions.User;
+import OptionsPackage.UserOptionsController;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+import OptionsPackage.SearchController;
+import OptionsPackage.ListOfProductsPresenter;
 
 public class ListOfProductsPresenterTest {
+    User user = new User("test1");
+    ListOfProductsPresenter LOPP;
+
+    @BeforeEach
+    void setUp(){
+        LOPP = new ListOfProductsPresenter();
+    }
+
+    @Test
+    public void testForEmpty() throws IOException, ClassNotFoundException {
+        List<String> list = Collections.emptyList();
+        LOPP.presentTagList(list);
+    }
+
+    @Test
+    public void testWithProduct() throws IOException, ClassNotFoundException {
+        List<String> list = new ArrayList<String>("test1");
+        LOPP.presentTagList(list);
+    }
+
 }
 //
