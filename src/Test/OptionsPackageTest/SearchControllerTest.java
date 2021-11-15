243c5f85b23114a8726c791c5d29c63469639c6a
Diego <diego.gonzalezdelgado@mail.utoronto.ca>
2021-11-14 21:32:57 -0500

    test files

--- src/Test/OptionsPackageTest/SearchControllerTest.java
+++ src/Test/OptionsPackageTest/SearchControllerTest.java
@@ -2,39 +2,76 @@ package OptionsPackageTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 //
+import InputAndOutput.SystemInOut;
+import PostFunctions.Post;
+import ProductFunctions.Product;
+import loginTest.SignInControllerTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import UserFunctions.User;
 import OptionsPackage.UserOptionsController;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import OptionsPackage.SearchController;
 
 public class SearchControllerTest{
     SearchController searchController;
     User user;
-    UserOptionsController userOption;
+    SystemInOutTest inOut;
+
 
     @BeforeEach
     void setUp(){
         user = new User("test1");
         searchController = new SearchController(user);
-        userOption = new UserOptionsController(user);
+        {
+            try {
+                inOut = new SystemInOutTest("src/Test/OptionsPackageTest/UserOptionsTest");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Test
     public void testNotInSearch() throws IOException, ClassNotFoundException {
-        ;
+        searchController.allowSearch();
+        inOut.getInput();
     }
-
-
-    void testThrowsException() throws IOException, ClassNotFoundException{
-        ;
+    @Test
+    public void testInSearch() throws IOException, ClassNotFoundException{
+        Product product = new Product("test1", "test1", 1.0, "test1", 1);
+        Post post = new Post(product, "test1", false, true);
+        searchController.allowSearch();
+        inOut.getInput();
     }
+    private static class SystemInOutTest extends SystemInOut {
 
-    void testInSearch() throws IOException, ClassNotFoundException{
-        ;
+        private final Scanner reader;
+
+        /**
+         *
+         * @param fileName
+         * @throws FileNotFoundException
+         */
+        public SystemInOutTest(String fileName) throws FileNotFoundException {
+            File fileToRead = new File(fileName);
+            this.reader = new Scanner(fileToRead);
+        }
+
+        @Override
+        public String getInput() throws IOException {
+            if (reader.hasNextLine()) {
+                return this.reader.nextLine();
+            }
+            else{
+                return "";
+            }
+        }
     }
 }
--- src/Test/OptionsPackageTest/UserOptionsUseCaseTest.java
+++ src/Test/OptionsPackageTest/UserOptionsUseCaseTest.java
@@ -1,5 +1,56 @@
 package OptionsPackageTest;
 
+import InputAndOutput.SystemInOut;
+
+import OptionsPackage.UserOptionsUseCase;
+
+import UserFunctions.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+
 public class UserOptionsUseCaseTest {
+    UserOptionsUseCase useCase;
+    User user;
+
+    @BeforeEach
+    void setUp() throws FileNotFoundException {
+
+        user = new User("test1");
+        useCase = new UserOptionsUseCase(user);
+    }
+
+    @Test
+    public void testThrowsException(){
+        assertThrows(IOException.class, () -> useCase.userInput("6"));
+    }
+
+    private static class SystemInOutTest extends SystemInOut {
+
+        private final Scanner reader;
+
+        /**
+         *
+         * @param fileName
+         * @throws FileNotFoundException
+         */
+        public SystemInOutTest(String fileName) throws FileNotFoundException {
+            File fileToRead = new File(fileName);
+            this.reader = new Scanner(fileToRead);
+        }
+
+        @Override
+        public String getInput() throws IOException {
+            if (reader.hasNextLine()) {
+                return this.reader.nextLine();
+            }
+            else{
+                return "";
+            }
+        }
+    }
 }
-//
