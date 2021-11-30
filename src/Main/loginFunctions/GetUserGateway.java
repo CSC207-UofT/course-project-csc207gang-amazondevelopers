89bdde6fd4ff70b6fe72a020144c58b5e8ff2f6d
Diego <diego.gonzalezdelgado@mail.utoronto.ca>
2021-11-30 17:35:25 -0500

    Fixed issue with password being prompted when attempting to follow a user
    diego.gonzalezdelgado@mail.utoronto.ca

--- src/Main/loginFunctions/GetUserGateway.java
+++ src/Main/loginFunctions/GetUserGateway.java
@@ -29,10 +29,9 @@ public class GetUserGateway implements  SignInGatewayInterface {
         }
         // access the serialized file for this user.
         DictionaryReadWriter rw = new DictionaryReadWriter();
-        PasswordPresenter passwordPresenter = new PasswordPresenter();
+
         HashMap<String, Object> usersSavedDict = rw.readFromFile("src/Main/user.ser");
         if (usersSavedDict.containsKey(username)){
-            passwordPresenter.promptPasswordSignIn((User)usersSavedDict.get(username));
             return (User)usersSavedDict.get(username);
         }
         // if the user does not exist, return a user with an empty username, which the empty username is unaccepted
