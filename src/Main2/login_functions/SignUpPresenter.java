3e468e353ff40b9c5e9ba15f812fdbb1fbea94d1
Albert Luo <aj.luo@mail.utoronto.ca>
2021-12-01 19:14:36 -0500

    implemented OptionsController, not finished the actionPerformed method yet.

--- src/Main2/login_functions/SignUpPresenter.java
+++ src/Main2/login_functions/SignUpPresenter.java
@@ -6,4 +6,10 @@ public class SignUpPresenter {
      * @return returns a message for the reader to see
      */
     public String message(){return "Sign up for Bazaar!";}
+
+    /**
+     * A message to the user that the username is taken
+     * @return returns a message to reader to see that username is taken
+     */
+    public String message2(){return "This username is taken, please enter another one!";}
 }
