d705ed335e1eb219063d7b1f96c55cdd239e1e3c
Albert Luo <aj.luo@mail.utoronto.ca>
2021-12-03 16:08:15 -0500

    put messages for user into singupPresenter to link with signupGUI

--- src/Main2/login_functions/SignUpPresenter.java
+++ src/Main2/login_functions/SignUpPresenter.java
@@ -14,14 +14,39 @@ public class SignUpPresenter {
     public String message2(){return "This username is taken, please enter another one!";}
 
     /**
-     *
      * @return a message if the user was made
      */
     public String message3(){return "success!";}
 
     /**
-     *
      * @return if the password or userID field is empty
      */
     public String message4(){return "You should input all required fields!";}
+
+    /**
+     * @return string for button used to signup
+     */
+    public String signUp(){return "Sign Up";}
+
+    /**
+     * @return string for button used to reset username and password
+     */
+    public String reset(){return "Reset";}
+
+    /**
+     * @return string for button used to go back to Welcome Page
+     */
+    public String back(){return "Back";}
+
+    /**
+     * @return string to prompt user to enter username
+     */
+    public String userID (){return "userID: ";}
+
+    /**
+     *
+     * @return string to prompt user to enter password
+     */
+    public String password(){return "password: ";}
 }
+
