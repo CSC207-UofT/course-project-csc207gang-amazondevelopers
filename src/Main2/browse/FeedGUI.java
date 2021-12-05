86739ccf7e0776b64e29e44a81a7e040bdf5a7a5
Yasamin <yasamin.nourijelyani@mail.utoronto.ca>
2021-12-05 17:22:41 -0500

    allow for users to add to cart bug fix

--- src/Main2/browse/FeedGUI.java
+++ src/Main2/browse/FeedGUI.java
@@ -135,5 +135,5 @@ public class FeedGUI implements ActionListener {
         }
 
     }
-}
+
 
--- src/Main2/options/search/ScrollSearchGui.java
+++ src/Main2/options/search/ScrollSearchGui.java
@@ -131,7 +131,7 @@ public class ScrollSearchGui implements ActionListener {
                 messageLabel.setForeground(Color.red);
                 messageLabel.setText(searchPresenter.cannotBuy());
 
-                frame.dispose();
+
 
             }
 
