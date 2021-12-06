138e014d3c60aa4da39d19600c0f0417f310f670
Jacqueline Zhu <jacqueline.zhu@mail.utoronto.ca>
2021-12-06 10:51:33 -0500

    changed name of addedtocart presenyer interface to be consistent with other interfaces

--- src/Main2/options/browse/AddedToCartPresenter/EnglishAddedPresenter.java
+++ src/Main2/options/browse/AddedToCartPresenter/EnglishAddedPresenter.java
@@ -1,6 +1,6 @@
 package options.browse.AddedToCartPresenter;
 
-public class EnglishAddedPresenter implements AddedToCartPresenter {
+public class EnglishAddedPresenter implements AddedToCartPresenterInterface {
     @Override
     public String presentAddedToCart() {
         return " was added to your options.cart.";
--- src/Test/followFunctionsTest/FollowControllerTest.java
+++ src/Test/followFunctionsTest/FollowControllerTest.java
@@ -1,6 +1,12 @@
 package followFunctionsTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class FollowControllerTest {
+
+
 
 
 
