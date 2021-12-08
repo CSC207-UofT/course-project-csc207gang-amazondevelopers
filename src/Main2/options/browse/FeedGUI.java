caf91d16c3abc436ad5efe6e4bcb21a019618f62
Jacqueline Zhu <jacqueline.zhu@mail.utoronto.ca>
2021-12-08 12:17:55 -0500

    copied code from conflicting merge request

--- src/Main2/options/browse/FeedGUI.java
+++ src/Main2/options/browse/FeedGUI.java
@@ -1,55 +1,80 @@
 package options.browse;
 
+import gui.ButtonCommandInterface;
+import gui.GUIFactoryInterface;
 import options.browse.FeedPresenter.EnglishFeedPresenter;
-
-import options.OptionsGUI;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import options.browse.commands.AddToCartCommand;
+import options.browse.commands.BackCommand;
+import options.browse.commands.NextCommand;
 import options.post.Post;
 import product.GetProductGateway;
 import product.Product;
 import user.User;
-import user.UserUseCase;
 
 /**
  * Class that presents a options.post to a user using our JFrame command line interface, allowing
  * a user to view the next post, view the last post or add to cart
  *
  */
-public class FeedGUI implements ActionListener {
-    JFrame frame = new JFrame();
-    EnglishFeedPresenter feedPresenter = new EnglishFeedPresenter();
-    JButton nextButton = new JButton(feedPresenter.presentNext());
-    JButton cartButton = new JButton(feedPresenter.presentBuy());
-    JButton backButton = new JButton(feedPresenter.presentBack());
-    JLabel posterLabel = new JLabel();
-    JLabel captionLabel = new JLabel();
-    JLabel productNameLabel = new JLabel();
-    JLabel productDescriptionLabel = new JLabel();
-    JLabel productQuantityLabel = new JLabel();
-    JLabel productSizeLabel = new JLabel();
-    JLabel indexLabel = new JLabel();
+public class FeedGUIMaker implements ActionListener, GUIFactoryInterface {
     ArrayList<Post> feed;
     Post post;
     int index;
     User user;
     Product product;
+    static Map<String, ButtonCommandInterface> commandMap = new HashMap<>();
+    JFrame frame = new JFrame();
 
     /**
      * Represents a constructor for the JFrame of a Post in the Feed
      * @param feed        Represents the total feed of the current user
      * @param user        The user viewing posts
      */
-    public FeedGUI( ArrayList<Post> feed, User user, int index){
-        this.post = feed.get(index);
+    public FeedGUIMaker(ArrayList<Post> feed, User user, int index){
         this.feed = feed;
         this.index = index;
         this.user = user;
+    }
+
+    /**
+     * Represents different actions when the JButtons are interacted with the user
+     * @param action the Action event performed in
+     */
+    @Override
+    public void actionPerformed(ActionEvent action) {
+        String buttonText = action.getActionCommand();
+        ButtonCommandInterface button = commandMap.get(buttonText);
+        try {
+            button.apply();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void createGUI() {
+
+        EnglishFeedPresenter feedPresenter = new EnglishFeedPresenter();
+        JButton nextButton = new JButton(feedPresenter.presentNext());
+        JButton cartButton = new JButton(feedPresenter.presentBuy());
+        JButton backButton = new JButton(feedPresenter.presentBack());
+        JLabel posterLabel = new JLabel();
+        JLabel captionLabel = new JLabel();
+        JLabel productNameLabel = new JLabel();
+        JLabel productDescriptionLabel = new JLabel();
+        JLabel productQuantityLabel = new JLabel();
+        JLabel productSizeLabel = new JLabel();
+        JLabel indexLabel = new JLabel();
+        this.post = feed.get(index);
         GetProductGateway getProductGateway = new GetProductGateway();
         this.product = getProductGateway.getProduct(post.getProduct());
         posterLabel.setText(feedPresenter.presentPostedBy() + post.getUser().getUsername());
@@ -92,43 +117,10 @@ public class FeedGUI implements ActionListener {
         frame.setSize(500, 500);
         frame.setLayout(null);
         frame.setVisible(true);
-    }
 
-    /**
-     * Represents different actions when the JButtons are interacted with the user
-     * @param e the Action event performed in
-     */
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == nextButton) {
-            if (index == feed.size() - 1) {
-                frame.setVisible(false);
-                frame.dispose();
-                OptionsGUI optionsGateway = new OptionsGUI(user);
-            } else {
-                frame.setVisible(false);
-                frame.dispose();
-                FeedGUI feedGUI = new FeedGUI(feed, user, index + 1);
-            }
-        }
-        if (e.getSource() == backButton) {
-            if (this.index == 0) {
-                frame.setVisible(false);
-                frame.dispose();
-                OptionsGUI optionsGUI = new OptionsGUI(user);
-            } else {
-                frame.setVisible(false);
-                frame.dispose();
-                FeedGUI feedGUI = new FeedGUI( feed, user, index - 1);
-            }
-        }
-        if (e.getSource() == cartButton) {
-            UserUseCase userUseCase = new UserUseCase(user);
-            userUseCase.userAddToCart(product);
-            frame.setVisible(false);
-            frame.dispose();
-            AddedToCartGUI addedToCartGUI = new AddedToCartGUI(user, product.getName());
-        }
+        commandMap.put(nextButton.getText(), new NextCommand(this.user, frame, feed, index));
+        commandMap.put(backButton.getText(), new BackCommand(this.user, frame, feed, index));
+        commandMap.put(cartButton.getText(), new AddToCartCommand(product, this.user, frame));
     }
 }
 
