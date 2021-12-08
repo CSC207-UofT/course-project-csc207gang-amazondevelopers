4d4e50bc9b4b53bfc536ff2f2459d24e64066205
Yasamin <yasamin.nourijelyani@mail.utoronto.ca>
2021-12-08 10:24:20 -0500

    user tests added

--- src/Test/userTest/UserTest.java
+++ src/Test/userTest/UserTest.java
@@ -1,4 +1,17 @@
 package userTest;
 
+import org.junit.Test;
+import user.User;
+
+import static org.junit.Assert.*;
+
+
 public class UserTest {
+
+    @Test
+    public void createUserTest(){
+        User user = new User("Yasmin");
+        assertEquals("Yasmin", user.getUsername());
+    }
+
 }
