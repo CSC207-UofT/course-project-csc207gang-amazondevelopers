ed5a9c860f56af45c6eb58b01867c44a01264366
Jacqueline Zhu <jacqueline.zhu@mail.utoronto.ca>
2021-12-08 23:20:44 -0500

    fixed search tests

--- src/Test/search_test/SearchControllerTest.java
+++ src/Test/search_test/SearchControllerTest.java
@@ -22,60 +22,63 @@ import static org.junit.Assert.assertEquals;
 public class SearchControllerTest{
 
     User testUser = new User("TestSearchController");
-    Product testProduct = new Product("test", "TEST", 5.0, "test", "2",1);;
+    Product testProduct = new Product("testProd", "TEST", 5.0, "testCategory", "2",1);;
     SearchController searchController = new SearchController(testUser);
 
     SaveProductGateway saveProductGateway = new SaveProductGateway();
-    GetProductGateway getProductGateway = new GetProductGateway();
     SaveUserGateway saveUserGateway = new SaveUserGateway();
-    GetUserGateway getUserGateway = new GetUserGateway();
 
     DeleteProductsGateway deleteProductsGateway = new DeleteProductsGateway();
     DeleteUserGateway deleteUserGateway = new DeleteUserGateway();
 
     @Before
     public void setUp() throws IOException, ClassNotFoundException {
-
         // if there is a preexisting user TestSearchControllerUser, delete it
         deleteUserGateway.deleteUser("TestSearchControllerUser");
-        ArrayList<String> ids = new ArrayList<>();
-        ids.add("TEST");
-        deleteProductsGateway.deleteProducts(ids);
         // create the new user profile before each test
         saveUserGateway.saveUser("TestSearchController", testUser);
-        // save a product to the repo to test with
-        saveProductGateway.addProductToRepo(testProduct, "TEST", "test");
     }
 
     @After
-    public void takeDown() throws IOException, ClassNotFoundException {
+    public void takeDown() {
         // delete the test user profile before each test, to delete all the products that were created during the test
         deleteUserGateway.deleteUser("TestSearchControllerUser");
-        ArrayList<String> ids = new ArrayList<>();
-        ids.add("TEST");
-        deleteProductsGateway.deleteProducts(ids);
     }
 
     //  name + " (" + id + ")" + ": $" + price + ", " + quantity + " in stock";
     //  name + " (" + id + ")" + ": $" + price + ", " + quantity + " in stock" + ", " + sizes;
     // "shoes", "TEST", 5.0, "shoes", "2",1
     @Test
-    public void getProductIDTest() throws IOException, ClassNotFoundException {
-        assertEquals("(0) test (TEST): $5.0, 1 in stock, 2", searchController.getSearchProductStrings("test").get(0));
+    public void getProductIDTest() {
+        // save a product to the repo to test with
+        saveProductGateway.addProductToRepo(testProduct, "TEST", "testCategory");
+        assertEquals("0) testProd (TEST): $5.0, 1 in stock, size: 2", searchController.getSearchProductStrings("testCategory").get(0));
+        ArrayList<String> ids = new ArrayList<>();
+        ids.add("TEST");
+        deleteProductsGateway.deleteProducts(ids);
     }
 
     @Test
-    public void getProductIDMultipleTest() throws IOException, ClassNotFoundException {
-        saveProductGateway.addProductToRepo(testProduct, "TEST", "test");
+    public void getProductIDMultipleTest() {
+        // save a product to the repo to test with
+        Product product1 = new Product("testProd1", "TEST1", 5.0, "testCategory", "2", 1);
+        Product product2 = new Product("testProd2", "TEST2", 5.0, "testCategory", "2", 1);
+        saveProductGateway.addProductToRepo(product1, "TEST1", "testCategory");
+        saveProductGateway.addProductToRepo(product2, "TEST2", "testCategory");
 
-        assertEquals(searchController.getSearchProductStrings("test").size(), 2);
-        ArrayList<String> ids = searchController.getSearchProductStrings("shoes");
-        assertEquals("(0) test (TEST): $5.0, 1 in stock, 2", ids.get(0));
-        assertEquals("(1) test (TEST): $5.0, 1 in stock, 2", ids.get(1));
+        assertEquals(searchController.getSearchProductStrings("testCategory").size(), 2);
+        ArrayList<String> ids = searchController.getSearchProductStrings("testCategory");
+        assertEquals("0) testProd1 (TEST1): $5.0, 1 in stock, size: 2", ids.get(0));
+        assertEquals("1) testProd2 (TEST2): $5.0, 1 in stock, size: 2", ids.get(1));
+
+        ArrayList<String> ids2 = new ArrayList<>();
+        ids2.add("TEST1");
+        ids2.add("TEST2");
+        deleteProductsGateway.deleteProducts(ids2);
     }
 
     @Test
-    public void getProductIDNoProductTest() throws IOException, ClassNotFoundException {
+    public void getProductIDNoProductTest() {
         assertEquals(0, searchController.getSearchProductStrings("jadshjasdjsah").size());
     }
 
